9a00fa4c8bb1356c106cfbedfb251d9284a9ae32
Andrew Howell <andrewhowell@stripe.com>
2020-07-27 21:28:37 -0700

    First commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,67 @@
 import React from 'react';
-import logo from './logo.svg';
+import { 
+  Navbar,
+  Nav,
+  Card,
+  Button 
+} from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import margherita from './margherita.jpg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar bg="light" expand="lg">
+        <Navbar.Brand href="#home">Allora</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+      <Container>
+        <Card style={{ width: '18rem' }}>
+        <Card.Img variant="top" src={margherita} />
+        <Card.Body>
+          <Card.Title>Pizza Margherita</Card.Title>
+          <Card.Text>
+            Is there anything more classic? One bite and you'll be transported to Naples where it all began.
+            <br/>
+            <br/>
+            Includes 1 dough ball, 2 oz sauce, 4 oz DOP buffalo mozzarella, and fresh basil sprigs.
+          </Card.Text>
+          <Button variant="primary">Go somewhere</Button>
+          </Card.Body>
+        </Card>
+        <Card style={{ width: '18rem' }}>
+        <Card.Img variant="top" src={margherita} />
+        <Card.Body>
+          <Card.Title>Pizza Margherita</Card.Title>
+          <Card.Text>
+            Is there anything more classic? One bite and you'll be transported to Naples where it all began.
+            <br/>
+            <br/>
+            Includes 1 dough ball, 2 oz sauce, 4 oz DOP buffalo mozzarella, and fresh basil sprigs.
+          </Card.Text>
+          <Button variant="primary">Go somewhere</Button>
+          </Card.Body>
+        </Card>
+        <Card style={{ width: '18rem' }}>
+        <Card.Img variant="top" src={margherita} />
+        <Card.Body>
+          <Card.Title>Pizza Margherita</Card.Title>
+          <Card.Text>
+            Is there anything more classic? One bite and you'll be transported to Naples where it all began.
+            <br/>
+            <br/>
+            Includes 1 dough ball, 2 oz sauce, 4 oz DOP buffalo mozzarella, and fresh basil sprigs.
+          </Card.Text>
+          <Button variant="primary">Go somewhere</Button>
+          </Card.Body>
+        </Card>
+      </Container>
     </div>
   );
 }
